7557967419e00d86b106951068a35a534fbd8d9f
Rahul Prakash <rahulprakash0898@gmail.com>
2024-12-02 17:46:00 +0530

    extra

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello all welcome to react application</h1>
+        <h2>Hello Welcome to devops</h2>
         {/* <p>by VSCharan</p> */}
       </header>
     </div>
